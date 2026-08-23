0430ed70a4643067bdc2f5b0546a9e96a1ecc212
Nicholas Chang <nicholaschangck@gmail.com>
2026-08-23 16:08:48 +0800

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return nums[0]
+        dp = defaultdict(int)
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+
+        for i in range(2,len(nums)):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        return dp[len(nums) - 1]
