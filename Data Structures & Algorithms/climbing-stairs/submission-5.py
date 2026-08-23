0d656914bc93d5164bc2e85f88caa29ed48ba757
Nicholas Chang <nicholaschangck@gmail.com>
2026-08-23 15:14:00 +0800

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = defaultdict(int)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
