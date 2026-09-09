4fedd983e3c36f40a5fd2c0b918cfd24d82e0a4c
Nicholas Chang <nicholaschangck@gmail.com>
2026-09-09 22:20:43 +0800

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def dfs(course, visited):
+            if course in visited:
+                return False
+            if path[course] == []:
+                return True
+            visited.add(course)
+            for prereq in path[course]:
+                if not dfs(prereq, visited):
+                    return False
+            visited.remove(course)
+            path[course] = []
+            return True
+        
+        path = {i: [] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            path[course].append(prereq)
+        
+        visited = set()
+        for key in path:
+            visited.add(key)
+            for prereq in path[key]:
+                if not dfs(prereq, visited):
+                    return False
+            visited.remove(key)
+        return True
